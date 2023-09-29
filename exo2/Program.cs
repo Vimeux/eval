463822eccfd79c5777044ee687d8c21dec4f96be
Benjamin Vimeux <vimeux.benji@gmail.com>
2023-09-29 10:47:14 +0200

    first commit

---
+++ exo2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter a string to convert to upper case:");
+        string stringInput = Console.ReadLine();
+        Console.WriteLine(ConvertToUpper(stringInput));
+    }
+
+    public static string ConvertToUpper(string text)
+    {
+        return text.ToUpper();
+    }
+}
---
+++ exo4/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Program
+{
+  static List<char> alphabet = new List<char>
+    {
+        'a','b','c','d','e','f','g','h','i','j','k','l','m',
+        'n','o','p','q','r','s','t','u','v','w','x','y','z'
+    };
+
+  public static void Main()
+  {
+    AlgoTest(MyNameIs);
+    Console.ReadLine();
+  }
+
+  public static string MyNameIs(string name)
+  {
+    string result = "";
+
+    foreach (char letter in name.ToLower())
+    {
+      if (alphabet.Contains(letter))
+      {
+        result += alphabet.IndexOf(letter);
+      }
+    }
+    return result;
+  }
+
+  public static void AlgoTest(Func<string, string> func)
+  {
+    if (func("AB") == "01")
+    {
+      Console.WriteLine("Success");
+    }
+    else
+    {
+      Console.WriteLine("Fail");
+    }
+  }
+}
